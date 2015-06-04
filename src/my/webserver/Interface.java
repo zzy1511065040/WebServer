2b32e2406ab52140c14e95b7253669e48d4b7a45
glkwhr <glkwhr@126.com>
2015-06-04 21:43:33 +0800

    v0.2.0	实现非法输入处理，实现服务启停状态转换

--- src/my/webserver/Interface.java
+++ src/my/webserver/Interface.java
@@ -10,6 +10,7 @@ import javax.swing.border.EmptyBorder;
 
 
 
+
 //jgoodies主题包
 import com.jgoodies.looks.plastic.PlasticLookAndFeel;
 //import com.jgoodies.looks.windows.WindowsLookAndFeel;
@@ -31,25 +32,25 @@ public class Interface {
 		});//magic
 	}
 	/*
-	 * 获取当前ip输入框的文本
+	 * 获取应用的ip
 	 */
 	public String getIPAddr()
 	{
-		return frame.ipText.getText();
+		return frame.curIPAddr;
 	}
 	/*
-	 * 获取当前port输入框的文本
+	 * 获取应用的端口
 	 */
-	public String getPort()
+	public int getPort()
 	{
-		return frame.portText.getText();
+		return frame.curPort;
 	}
 	/*
-	 * 获取当前folder框的文本
+	 * 获取应用的目录
 	 */
 	public String getFolder()
 	{
-		return frame.folderText.getText();
+		return frame.curDir;
 	}
 	/*
 	 * 获得logText
@@ -105,24 +106,31 @@ class MyFrame extends JFrame{
 		
 		//ip文本框
 		JLabel ipLabel = new JLabel("IP");
-		ipText = new JTextArea();
-		ipText.setText("127.0.0.1");
+		ipText = new JTextField("127.0.0.1");
 		ipText.setEditable(true);
-		ipText.setLineWrap(false);
 		ipText.setBorder(BorderFactory.createEtchedBorder());
+		ipText.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				applyBtn.setEnabled(true);
+			}
+		});
 		//port文本框
 		JLabel portLabel = new JLabel("Port");
-		portText = new JTextArea();
-		portText.setText("6789");
+		portText = new JTextField("6789");
 		portText.setEditable(true);
-		portText.setLineWrap(false);
 		portText.setBorder(BorderFactory.createEtchedBorder());
+		portText.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				applyBtn.setEnabled(true);
+			}
+		});
 		//folder文本框
 		JLabel folderLabel = new JLabel("Folder");
 		folderText = new JTextArea();
 		//folderText.setText("Choose main folder");
 		folderText.setEditable(false);
-		folderText.setLineWrap(false);
 		folderScrollPane = new JScrollPane(folderText);
 		folderScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		folderScrollPane.setBorder(BorderFactory.createEtchedBorder());//设置滚动栏边框
@@ -152,6 +160,7 @@ class MyFrame extends JFrame{
 					sltDir = folderChooser.getSelectedFile().toString();
 					folderText.setText(sltDir);
 				}
+				applyBtn.setEnabled(true);
 			}
 		});
 		//log文本框（输出）
@@ -174,9 +183,76 @@ class MyFrame extends JFrame{
 		//应用当前设置按钮
 		applyBtn = new JButton("<html><font size=2>Apply</font></html>");
 		applyBtn.setPreferredSize(new Dimension(60, 25));
+		applyBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//点击apply时将当前的设置应用
+				if( isIP(ipText.getText()) )
+				{	//如果IP框中输入的是合法ip地址格式
+					goodIP = true; //IP合法
+					curIPAddr = ipText.getText();
+				}
+				else
+				{
+					goodIP = false; //IP不合法
+					ipText.setText("");
+					JOptionPane.showMessageDialog(	JOptionPane.getRootFrame(), 
+												  	"Invalid IP address!", 
+												  	"ERROR", 
+												  	JOptionPane.ERROR_MESSAGE);					
+				}
+				int temp;
+				if( portText.getText().length() < 7
+						&& portText.getText().matches("[0-9]+")
+						&& (temp = Integer.parseInt(portText.getText().trim())) > 0 
+						&& temp < 65536 )
+				{	//如果Port框中输入的是合法port
+					goodPort = true; //port合法
+					curPort = temp;
+				}
+				else
+				{
+					goodPort = false; //port不合法
+					portText.setText("");
+					JOptionPane.showMessageDialog(	JOptionPane.getRootFrame(), 
+													"Invalid port number!", 
+													"ERROR", 
+													JOptionPane.ERROR_MESSAGE);
+				}
+				curDir = sltDir + "\\";
+				if(setFlag = goodIP && goodPort)
+					//如果ip port皆合法则设置应用成功
+					applyBtn.setEnabled(false);
+			}
+		});
 		//启动按钮
-		startBtn = new JButton("<html><font size=2>Start</font></html>");
+		startBtn = new JButton("<html><b><font size=3 color=green>Start</font></b></html>");
 		startBtn.setPreferredSize(new Dimension(60, 25));
+		startBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(runFlag)
+				{	//如果服务正在进行，则此时为stop按钮
+					runFlag = false; //终止服务
+					startBtn.setText("<html><b><font size=3 color=green>Start</font></b></html>");
+					logText.append("Stoping...\n");
+				}
+				else
+				{	//如果服务不在进行，则此时为start按钮
+					if(setFlag)
+					{	//若已经设置ip和port
+						logText.append("Starting...\n");
+						runFlag = true; //启动服务
+						startBtn.setText("<html><b><font size=3 color=red>Stop</font></b></html>");
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(	JOptionPane.getRootFrame(), 
+								"Apply your settings first!", 
+								"ERROR", 
+								JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		});
 		
 		//将组件加入网格, 用到GBC帮助类(GBC.java)
 		contentPane.add(ipLabel, new GBC(0, 0).setAnchor(GBC.WEST).setInsets(0,5,0,0));
@@ -204,9 +280,25 @@ class MyFrame extends JFrame{
             SwingUtilities.updateComponentTreeUI(this);
         } catch (Exception e) {}
 	}
+	
+	private boolean isIP(String text)
+	{
+		 if (text != null && !text.isEmpty()) {
+	            // 定义正则表达式
+	            String regex = "^(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|[1-9])\\."
+	                    	 + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+	                    	 + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+	                    	 + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)$";
+	            // 判断ip地址是否与正则表达式匹配
+	            if (text.matches(regex))
+	            	return true;
+		 }
+		return false;
+	}
+	
 	private JPanel contentPane;
-	public JTextArea ipText;		//ip文本
-	public JTextArea portText;		//端口文本
+	public JTextField ipText;		//ip文本
+	public JTextField portText;		//端口文本
 	public JTextArea folderText;	//文件路径文本
 	public JTextArea logText;		//日志文本
 	private JScrollPane logScrollPane;	//滚动条面板
@@ -218,7 +310,17 @@ class MyFrame extends JFrame{
 	private JMenu setting;
 	private JMenu help;
 	private JFileChooser folderChooser;
+	
+	public String curIPAddr;	//当前应用的ip
+	public int curPort;		//当前应用的端口
+	public String curDir;		//当前应用的目录
 	private String sltDir;
+	
+	public boolean runFlag = false;		//服务正在进行
+	public boolean setFlag = false;		//已设置好ip和端口
+	private boolean goodIP = true; 		//当前输入ip是否合法
+	private boolean goodPort = true;	//当前输入port是否合法
+	
 	final int WINDOW_WIDTH = 260;
 	final int WINDOW_HEIGHT = 360;
 }
