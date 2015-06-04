628c3e0fc652dce24525bb624fad8d6ba6a476a6
glkwhr <glkwhr@126.com>
2015-06-04 18:29:29 +0800

    v0.1.3 	实现路径选择和清空log文本框功能 		interface类增加方法以获取各文本框中值或写log框

--- src/my/webserver/Interface.java
+++ src/my/webserver/Interface.java
@@ -1,15 +1,21 @@
 package my.webserver;
 
 import java.awt.*;
-//import java.awt.event.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-//import com.jgoodies.looks.windows.WindowsLookAndFeel;
+
+
+//jgoodies主题包
 import com.jgoodies.looks.plastic.PlasticLookAndFeel;
+//import com.jgoodies.looks.windows.WindowsLookAndFeel;
 //import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
 //import com.jgoodies.looks.plastic.PlasticXPLookAndFeel;
-import com.jgoodies.looks.plastic.theme.*;//jgoodies主题包
+import com.jgoodies.looks.plastic.theme.*;
 
 public class Interface {
 	public Interface()
@@ -24,20 +30,54 @@ public class Interface {
 			}
 		});//magic
 	}
-	public MyFrame frame;
+	/*
+	 * 获取当前ip输入框的文本
+	 */
+	public String getIPAddr()
+	{
+		return frame.ipText.getText();
+	}
+	/*
+	 * 获取当前port输入框的文本
+	 */
+	public String getPort()
+	{
+		return frame.portText.getText();
+	}
+	/*
+	 * 获取当前folder框的文本
+	 */
+	public String getFolder()
+	{
+		return frame.folderText.getText();
+	}
+	/*
+	 * 向logText写入
+	 */
+	public void writeLog(String log)
+	{
+		frame.logText.append(log);
+		frame.logText.setCaretPosition(frame.logText.getDocument().getLength());
+		//设置光标到最后一行从而保持滚动条一直在最底端 
+	}
+	private MyFrame frame;
 }
 
 class MyFrame extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public MyFrame()
 	{
 		GridBagLayout layout;
 		
-		//set frame width, height and let platform pick screen location
-		setSize(260, 360);
+		//设置frame的宽度, 高度；由 platform选择窗口的位置；固定窗口大小
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		setLocationByPlatform(true);
 		setResizable(false);
 		
-		//set frame icon and title
+		//设置frame的icon和title
 		//Image img = kit.getImage("icon.gif");
 		//setIconImage(img);
 		setTitle("WebServer");
@@ -58,7 +98,7 @@ class MyFrame extends JFrame{
 		JMenuItem about = new JMenuItem("About");
 		help.add(about);
 		
-		//set content pane
+		//设置内容面板
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		add(contentPane);
@@ -66,59 +106,101 @@ class MyFrame extends JFrame{
 		layout = new GridBagLayout();
 		contentPane.setLayout(layout);
 		
+		//ip文本框
 		JLabel ipLabel = new JLabel("IP");
 		ipText = new JTextArea();
 		ipText.setText("127.0.0.1");
 		ipText.setEditable(true);
 		ipText.setLineWrap(false);
 		ipText.setBorder(BorderFactory.createEtchedBorder());
+		//port文本框
 		JLabel portLabel = new JLabel("Port");
 		portText = new JTextArea();
 		portText.setText("6789");
 		portText.setEditable(true);
 		portText.setLineWrap(false);
 		portText.setBorder(BorderFactory.createEtchedBorder());
+		//folder文本框
 		JLabel folderLabel = new JLabel("Folder");
 		folderText = new JTextArea();
-		folderText.setText("Choose main folder");
+		//folderText.setText("Choose main folder");
 		folderText.setEditable(false);
 		folderText.setLineWrap(false);
-		folderText.setBorder(BorderFactory.createEtchedBorder());
+		folderScrollPane = new JScrollPane(folderText);
+		folderScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		folderScrollPane.setBorder(BorderFactory.createEtchedBorder());//设置滚动栏边框
+		//folderScrollPane.setPreferredSize(new Dimension(220, 150));
 		folderBtn = new JButton("...");
+		try {
+			sltDir = new File(".").getCanonicalPath();//获得当前标准路径
+			folderText.setText(sltDir);//默认当前路径
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		//“浏览”按钮
 		folderBtn.setPreferredSize(new Dimension(20, 20));
+		folderBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//if (StFlag) {
+				//	JOptionPane.showMessageDialog(null, "服务器正在运行中，请关闭服务后再修改", "Error", 0);
+				//	return;
+				//}				
+				folderChooser = new JFileChooser();
+				folderChooser.setCurrentDirectory(new File("."));
+				folderChooser.setDialogTitle("Select a folder");
+				folderChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				folderChooser.setAcceptAllFileFilterUsed(false);				
+				if (folderChooser.showOpenDialog(folderBtn) == JFileChooser.APPROVE_OPTION) {
+					sltDir = folderChooser.getSelectedFile().toString();
+					folderText.setText(sltDir);
+				}
+			}
+		});
+		//log文本框（输出）
 		JLabel logLabel = new JLabel("Log ");
 		logText = new JTextArea();
 		logText.setWrapStyleWord(true);
 		logText.setLineWrap(true);
 		logText.setEditable(false);
-		scrollPane = new JScrollPane(logText);
-		scrollPane.setPreferredSize(new Dimension(220, 150));
+		//滚动条面板
+		logScrollPane = new JScrollPane(logText);
+		logScrollPane.setPreferredSize(new Dimension(220, 150));
+		//清空logText内容按钮
 		cleanBtn = new JButton("<html><font size=2>Clean</font></html>");
 		cleanBtn.setPreferredSize(new Dimension(60, 25));
+		cleanBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				logText.setText("");
+			}
+		});
+		//应用当前设置按钮
 		applyBtn = new JButton("<html><font size=2>Apply</font></html>");
 		applyBtn.setPreferredSize(new Dimension(60, 25));
+		//启动按钮
 		startBtn = new JButton("<html><font size=2>Start</font></html>");
 		startBtn.setPreferredSize(new Dimension(60, 25));
 		
-		//add components to grid, using GBC convenience class
+		//将组件加入网格, 用到GBC帮助类(GBC.java)
 		contentPane.add(ipLabel, new GBC(0, 0).setAnchor(GBC.WEST).setInsets(0,5,0,0));
 		contentPane.add(ipText, new GBC(1, 0).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(2));
 		contentPane.add(portLabel, new GBC(0, 1).setAnchor(GBC.WEST).setInsets(0,5,0,0));
 		contentPane.add(portText, new GBC(1, 1).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(2));
 		contentPane.add(folderLabel, new GBC(0, 2).setAnchor(GBC.WEST).setInsets(0,5,0,0));
-		contentPane.add(folderText, new GBC(1, 2).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(2));
+		contentPane.add(folderScrollPane, new GBC(1, 2).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(2));
 		contentPane.add(folderBtn, new GBC(2, 2).setAnchor(GBC.CENTER).setInsets(2,0,2,5));
 		contentPane.add(logLabel, new GBC(0, 3).setAnchor(GBC.WEST).setInsets(20,5,0,0));
-		contentPane.add(scrollPane, new GBC(0, 4).setSpan(3, 1).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(0,5,0,5));
+		contentPane.add(logScrollPane, new GBC(0, 4).setSpan(3, 1).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(0,5,0,5));
 		contentPane.add(cleanBtn, new GBC(0, 5).setAnchor(GBC.CENTER).setInsets(5,5,0,0));
 		contentPane.add(applyBtn, new GBC(1, 5).setAnchor(GBC.CENTER).setInsets(5,5,0,0));
 		contentPane.add(startBtn, new GBC(1, 5).setSpan(2, 1).setAnchor(GBC.EAST).setInsets(5,0,0,5));
 		
-		PlasticLookAndFeel.setPlasticTheme(new DesertBluer());//设置主题
+		//设置主题
+		PlasticLookAndFeel.setPlasticTheme(new DesertBluer());
         try {
             //设置观感
-            //UIManager.setLookAndFeel(new Plastic3DLookAndFeel());
             UIManager.setLookAndFeel("com.jgoodies.looks.windows.WindowsLookAndFeel");
+            //UIManager.setLookAndFeel(new Plastic3DLookAndFeel());
             //UIManager.setLookAndFeel("com.jgoodies.looks.plastic.PlasticLookAndFeel");
             //UIManager.setLookAndFeel("com.jgoodies.looks.plastic.Plastic3DLookAndFeel");
             //UIManager.setLookAndFeel("com.jgoodies.looks.plastic.PlasticXPLookAndFeel");
@@ -126,15 +208,20 @@ class MyFrame extends JFrame{
         } catch (Exception e) {}
 	}
 	private JPanel contentPane;
-	private JTextArea ipText;		//ip文本
-	private JTextArea portText;		//端口文本
-	private JTextArea folderText;	//文件路径文本
+	public JTextArea ipText;		//ip文本
+	public JTextArea portText;		//端口文本
+	public JTextArea folderText;	//文件路径文本
 	public JTextArea logText;		//日志文本
-	private JScrollPane scrollPane;	//滚动条面板
+	private JScrollPane logScrollPane;	//滚动条面板
+	private JScrollPane folderScrollPane;
 	private JButton folderBtn;		//“浏览”按钮
 	private JButton cleanBtn;		//清除log内容按钮
 	private JButton applyBtn;		//“应用”按钮
 	private JButton startBtn;		//“启动”按钮
 	private JMenu setting;
 	private JMenu help;
+	private JFileChooser folderChooser;
+	private String sltDir;
+	final int WINDOW_WIDTH = 260;
+	final int WINDOW_HEIGHT = 360;
 }
