1c7920ba0811bbeb37d2cb376ad46b6800fee91f
glkwhr <glkwhr@126.com>
2015-06-04 22:02:28 +0800

    测试用

--- src/my/webserver/WebServer.java
+++ src/my/webserver/WebServer.java
@@ -15,13 +15,11 @@ public class WebServer {
 		config = new Interface();
 		
 		ServerSocket welcomeSocket = 
-				new ServerSocket(6789, 0, InetAddress.getByName ("10.12.57.184"));//端口，队列长度，绑定地址
+				new ServerSocket(6789, 0, InetAddress.getByName ("127.0.0.1"));//端口，队列长度，绑定地址
 		while(true) {
 			Socket connectionSocket = welcomeSocket.accept();
 			clientSentence = connectionSocket.getInetAddress().toString();
-			config.frame.logText.append("Client IP:"+clientSentence.substring(1)+"\n");
-			config.frame.logText.setCaretPosition(config.frame.logText.getDocument().getLength());
-			//设置光标到最后一行从而保持滚动条一直在最底端 
+			//config.writeLog("Client IP:"+clientSentence.substring(1)+"\n");
 		}
 	}
 
